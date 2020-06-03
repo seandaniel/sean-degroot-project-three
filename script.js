ec89ec8915ba171b4dc15924a252e4eb48554eab
Sean de Groot <degroot.sd@gmail.com>
2020-06-02 23:08:56 -0400

    Added app. to all functions, cleaned up global variables

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 const app = {};
 
+// removing 'score :' and 'timer :'
+let $timer = ($('.timer').text()).slice(-2);
 let $score = ($('.score').text()).slice(-2);
-const $reset = ($('.reset'));
 
-const getUserWord = () => {
-  const userWord = ($('input').attr('placeholder'));
-  return userWord;
-}
-
-const getRandomWord = () => {
-  const randomWordArray = ['hippopotamus', 'arrays', 'hockey', 'loops', 'orbitz', 'grid', 'flex', 'wes bos', 'vanilla', 'elephant', 'extravagant', 'reptile', 'compelling', 'witty', 'turmoil', 'bicycle', 'rampage', 'peanuts', 'raptors', 'antidote', 'california', 'mozilla', 'snowboarding', 'golden', 'liberal', 'monotone', 'client', 'github', 'nutritional', 'puppy', 'rudolph', 'search', 'react', 'babel', 'sass', 'school', 'juno', 'friendship', 'alternate', 'withstand', 'salt', 'banana', 'montreal', 'creating', 'background', 'transition', 'special', 'country', 'holland', 'partake', 'eventually', 'fill', 'chiropractor', 'developer', 'phantom', 'switch', 'broke', 'keyboard', 'shelf', 'television', 'factory', 'volkswagen', 'multi'];
+// grabs random word
+app.getRandomWord = () => {
+  const randomWordArray = ['hippopotamus', 'arrays', 'hockey', 'loops', 'orbitz', 'grid', 'flex', 'wes bos', 'vanilla', 'elephant', 'extravagant', 'reptile', 'compelling', 'witty', 'turmoil', 'bicycle', 'rampage', 'peanuts', 'raptors', 'antidote', 'california', 'mozilla', 'snowboarding', 'golden', 'liberal', 'monotone', 'client', 'github', 'nutritional', 'puppy', 'rudolph', 'search', 'react', 'babel', 'sass', 'school', 'juno', 'friendship', 'alternate', 'withstand', 'salt', 'banana', 'montreal', 'creating', 'background', 'transition', 'special', 'country', 'holland', 'partake', 'eventually', 'fill', 'chiropractor', 'developer', 'phantom', 'switch', 'broke', 'keyboard', 'shelf', 'television', 'factory', 'volkswagen', 'multi', 'selfish', 'willing', 'corduroy', 'sandwich', 'sushi', 'survivor', 'calc', 'coding', 'colorado', 'friendship', 'courteous', 'patronize', 'philanthropy', 'reddit', 'netflix', 'medium', 'farewell'];
   const randomWord = randomWordArray[Math.floor(Math.random() * randomWordArray.length)];
   return randomWord;
 };
 
+app.startTimer = () => {
+  let highScore = $score;
 
-let $timer = ($('.timer').text()).slice(-2);
-
-const startTimer = () => {
   const minute = setInterval(function() {
     $timer--;
     $('.timer').text(`Timer: ${$timer}`);
@@ -26,76 +22,68 @@ const startTimer = () => {
       $('input').attr('disabled', true)
       .css('background-color', '#24292e');
       $('.reset').toggleClass('reset-hover');
+      $('.timer').addClass('game-over');
+      if ($score > highScore) {
+        highScore = $score;
+        $('.highscore').text(`Highscore: ${highScore}`);
+      }
     }
   }, 1000);
 }
 
-$('.reset').attr('disabled', true);
+app.submitWord = () => {
+  //or run this function once
+  if (parseInt($timer) === 10) {
+    app.startTimer();
+  }
 
-app.init = () => {
+  userWord = ($('input').val()).toLowerCase();
 
-  
-  randomWord = ($('input').attr('placeholder'));
-  
-  $('form').on('submit', (e) => {
-    e.preventDefault();
-    
-    $('.reset').toggleClass('reset-hover');
-    $('.reset').attr('disabled', false);
+  if (userWord === randomWord) {
+    $score++;
+    randomWord = app.getRandomWord();
+    $('input').attr('placeholder', randomWord);
+    $('.score').text(`Score: ${$score}`)
+      .css('color', '#f4f4f4');
+  } else {
+    $('.score').animate({
+      left: '100px',
+      color: '#f06464',
+    }, 350, function () {
+      $(this).css('left', '0');
+    });
+  }
 
+  $('input').val('');
+}
 
-    if (parseInt($timer) === 30) {
-      startTimer();
-    } 
+$('.reset').on('click', () => {
+  //disable start timer
 
-    userWord = ($('input').val());
-    console.log(userWord);
+  randomWord = 'start';
+  $score = 0;
+  $timer = 10;
 
-    if (userWord === randomWord) {
-      console.log('Correct');
-      $score ++;
-      randomWord = getRandomWord();
-      $('input').attr('placeholder', randomWord);
-      $('.score').text(`Score: ${$score}`);
-      $('.score').css('color', '#f4f4f4');
-    } else {
-      console.log('Incorrect')
+  $('.timer').removeClass('game-over')
+    .text('Timer: 10')
 
-      $('.score').animate({
-        left: '100px',
-        color: '#f06464',
-      }, 350, function () {
-        $(this).css('left', '0');
-      });
-
-    }
-
-    $('input').val('');
-
-  });
-
-  $reset.on('click', () => {
-    $('.reset').toggleClass('reset-hover');
-    randomWord = 'start';
-    $score = 0;
-    $timer = 30;
-
-    $('input')
+  $('input')
     .attr('disabled', false)
     .val('')
     .attr('placeholder', 'start');
 
-    $('.score').text('Score: 0')
+  $('.score').text('Score: 0')
     .css('color', '#f4f4f4');
-    $('.timer').text('Timer: 30');
+});
 
+
+app.init = () => {
+  randomWord = 'start';
+  
+  $('form').on('submit', (e) => {
+    e.preventDefault();
+    app.submitWord();
   });
-
-  // if ($timer === 30) {
-  //   startTimer().bind();
-  //   // reset event
-  // }
-
 };
 
 $(function() {
