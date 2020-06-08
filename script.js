562bf87f18ab68dcb08463f06ccc2437a8426986
Sean de Groot <degroot.sd@gmail.com>
2020-06-08 14:37:02 -0400

    Added namespace variables

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 const app = {};
 
+app.halfMinute;
+
 // removing 'score :' and 'timer :'
 let $timer = ($('.timer').text()).slice(-2);
 let $score = ($('.score').text()).slice(-2);
 
 app.getRandomWord = () => {
   const randomWordArray = ['hippopotamus', 'arrays', 'hockey', 'loops', 'orbitz', 'grid', 'flex', 'wes bos', 'vanilla', 'elephant', 'extravagant', 'reptile', 'compelling', 'witty', 'turmoil', 'bicycle', 'rampage', 'peanuts', 'raptors', 'antidote', 'california', 'mozilla', 'snowboarding', 'golden', 'liberal', 'monotone', 'client', 'github', 'nutritional', 'puppy', 'rudolph', 'search', 'react', 'babel', 'sass', 'school', 'juno', 'friendship', 'alternate', 'withstand', 'salt', 'banana', 'montreal', 'creating', 'background', 'transition', 'special', 'country', 'holland', 'partake', 'eventually', 'fill', 'chiropractor', 'developer', 'phantom', 'switch', 'broke', 'keyboard', 'shelf', 'television', 'factory', 'volkswagen', 'multi', 'selfish', 'willing', 'corduroy', 'sandwich', 'sushi', 'survivor', 'calc', 'coding', 'colorado', 'friendship', 'courteous', 'patronize', 'philanthropy', 'reddit', 'netflix', 'medium', 'farewell', 'salmonella', 'spatula', 'flipping', 'candid', 'gallery', 'foot', 'amsterdam', 'salad', 'tantalizing', 'coupled', 'print', 'fringe', 'clarity', 'project', 'sean', 'apple', 'tunes', 'spotify', 'time', 'clock', 'perfect', 'wine', 'wrist', 'mango', 'dumpster', 'agonize', 'score', 'flowers', 'russia', 'archery', 'bowling', 'baseball', 'cannon', 'frozen', 'bewildered', 'black', 'twitter', 'length', 'function', 'object', 'playlist', 'follow', 'rumble', 'catalyst', 'junk', 'pillow', 'chimpanzee', 'elephant', 'clarification', 'glass', 'headphones', 'shelf', 'pins', 'plant', 'cord', 'mug', 'lamp', 'pencil', 'book', 'marley', 'mammal', 'puppy', 'ralph', 'calibrate', 'consider', 'frowned', 'photography', 'cinematography', 'beautiful', 'worship', 'punish', 'extract', 'simplify', 'conduct', 'pursue', 'depend', 'cappucino', 'tale', 'sword', 'skate', 'compass', 'ritual', 'hacker', 'neat', 'interrogate', 'purchase', 'monopoly', 'microsoft', 'linguistics', 'slam', 'quarantine', 'hospital', 'employment', 'vent', 'percolate', 'variable', 'fetch', 'plankton', 'harry', 'police', 'admin', 'continuous', 'family', 'earth', 'mars','jupiter', 'suspension', 'afraid', 'momentum', 'potential', 'dice', 'vehicle', 'shipping', 'cart', 'frantic', 'riley','north', 'tantalize', 'spa', 'landscape', 'golf', 'automobile', 'pavement', 'potato', 'rim', 'tire', 'oil', 'jug', 'wooden', 'concrete', 'laminate', 'pursuit', 'rumble', 'consequence', 'sidewalk', 'crank', 'insufferable', 'platoon', 'perplexed', 'rudimentary', 'canine', 'simpleton', 'salvage', 'ruin', 'pineapple', 'purchase', 'poison', 'dilemma'];
-  return randomWord = randomWordArray[Math.floor(Math.random() * randomWordArray.length)];
+  return app.randomWord = randomWordArray[Math.floor(Math.random() * randomWordArray.length)];
 };
 
-let halfMinute;
-
 app.startTimer = () => {
-  let highScore = $score;
+  app.highScore = $score;
 
-  halfMinute = setInterval(function() {
+  app.halfMinute = setInterval(function() {
     $timer--;
     $('.timer').text(`Timer: ${$timer}`);
     if ($timer === 0){
       $('input').attr('disabled', true);
-      clearInterval(halfMinute);
+      clearInterval(app.halfMinute);
       $('.reset').toggleClass('reset-hover');
       $('.score').css('color', '#f4f4f4');
       $('.timer').addClass('game-over');
       // establishing a highscore
-      if ($score > highScore) {
-        highScore = $score;
-        $('.highscore').text(`Highscore: ${highScore}`);
+      if ($score > app.highScore) {
+        app.highScore = $score;
+        $('.highscore').text(`Highscore: ${app.highScore}`);
       }
     } 
   }, 1000);
 }
 
 app.submitWord = () => {
-  let userWord = ($('input').val()).toLowerCase();
+  app.userWord = ($('input').val()).toLowerCase();
 
   // prevents app.StartTimer() from running more than once
-  if (parseInt($timer) === 30 && userWord === 'start') {
+  if (parseInt($timer) === 30 && app.userWord === 'start') {
     app.startTimer();
   }
 
-  if (userWord === randomWord) {
+  if (app.userWord === app.randomWord) {
     $score++;
     randomWord = app.getRandomWord();
 
-    $('.correct-word').text(userWord);
-    $('input').attr('placeholder', randomWord);
+    $('.correct-word').text(app.userWord);
+    $('input').attr('placeholder', app.randomWord);
     $('.score').text(`Score: ${$score}`)
     .css('color', '#f4f4f4');
     
@@ -62,9 +62,9 @@ app.submitWord = () => {
 }
 
 $('.reset').on('click', () => {
-  clearInterval(halfMinute);
+  clearInterval(app.halfMinute);
 
-  randomWord = 'start';
+  app.randomWord = 'start';
   $score = 0;
   $timer = 30;
 
@@ -84,7 +84,7 @@ $('.reset').on('click', () => {
 
 
 app.init = () => {
-  randomWord = 'start';
+  app.randomWord = 'start';
 
   $('form').on('submit', (e) => {
     e.preventDefault();
